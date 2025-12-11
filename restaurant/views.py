010ccde50f55d40cb8e44b878aca3ab8c92fa774
Shadow <shadowxtrace@gmail.com>
2025-12-11 15:58:34 -0600

    Static & Templates download and structure, updated views, and urls

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    context = {
+        "current_year": datetime.now().year,
+    }
+    return render(request, "index.html", context)
+
